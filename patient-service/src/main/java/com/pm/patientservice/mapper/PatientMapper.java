8d3fada43edad5b5c112d2d828de8e9f527f3c35
Youssef Sghairi <sghairiyoussef10@gmail.com>
2025-04-12 16:47:20 +0100

    fix: null id problem fix

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -3,18 +3,16 @@ package com.pm.patientservice.mapper;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
-
 import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
-
-        patientDTO.setId(patientDTO.getId());
+        patientDTO.setId(patient.getId().toString());
         patientDTO.setName(patient.getName());
-        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
         patientDTO.setAddress(patient.getAddress());
         patientDTO.setEmail(patient.getEmail());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
     }
@@ -24,8 +22,8 @@ public class PatientMapper {
         patient.setName(patientRequestDTO.getName());
         patient.setAddress(patientRequestDTO.getAddress());
         patient.setEmail(patientRequestDTO.getEmail());
-        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
         patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
         return patient;
     }
 }
